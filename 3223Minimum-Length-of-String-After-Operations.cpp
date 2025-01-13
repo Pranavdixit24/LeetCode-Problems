97098993d1d125e044623ceb3ea494029f2f6696
Pranav <dixitpranav24@gmail.com>
2025-01-13 17:24:36 +0530

    added min len of string after operations

---
+++ 3223Minimum-Length-of-String-After-Operations.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+class Solution {
+public:
+    int minimumLength(string s) {
+        int n=s.size();
+        int result=0;
+        int deleted=0;
+        vector<int> freq(26,0);
+
+        for(int i=0;i<n;i++){
+            freq[s[i]-'a']++;
+            if(freq[s[i]-'a']==3){
+                freq[s[i]-'a']-=2;
+                deleted+=2;
+            }
+        }
+
+        return n-deleted;
+    }
+};
