4468488353a7b13558b870b8f4871ece54cdffdb
Pranav <dixitpranav24@gmail.com>
2025-01-28 20:34:38 +0530

    added prob 2658 max num of fish in grid bfs approach

--- 2658Maximum-Number-of-Fish-in-a-Grid.cpp
+++ 2658Maximum-Number-of-Fish-in-a-Grid.cpp
@@ -40,3 +40,54 @@ public:
         return maxFish;
     }
 };
+
+// ________________________________________
+
+class Solution {
+public:
+    //TC O(m*n) visiting each cell once 
+    //SC for bfs O(m*n) worst case
+    vector<vector<int>> directions={{-1,0},{1,0},{0,1},{0,-1}};
+    int bfs(int i,int j,vector<vector<int>>& grid){
+        int m=grid.size();
+        int n=grid[0].size();
+
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        int fishCount=grid[i][j];
+        grid[i][j]=0;
+
+        while(!q.empty()){
+            int i=q.front().first;
+            int j=q.front().second;
+            q.pop();
+
+            for(auto dir:directions){
+                int i_ = i + dir[0];
+                int j_ = j + dir[1];
+
+                if(i_>=0 && j_>=0 && i_<m && j_<n && grid[i_][j_]>0){
+                    q.push({i_,j_});
+                    fishCount += grid[i_][j_];
+                    grid[i_][j_]=0;
+
+                }
+
+            }
+        }
+        return fishCount;
+    }
+    int findMaxFish(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        int maxFish=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]>0){
+                    maxFish=max(maxFish,bfs(i,j,grid));
+                }
+            }
+        }
+        return maxFish;
+    }
+};
