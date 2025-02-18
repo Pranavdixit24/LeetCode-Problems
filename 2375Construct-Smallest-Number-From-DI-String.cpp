dbc32cd575ea6329b77f1c0700368d11f5f73b4b
Pranav <dixitpranav24@gmail.com>
2025-02-18 20:50:54 +0530

    added prob 2375 construct smallest number from DI string

---
+++ 2375Construct-Smallest-Number-From-DI-String.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        //TC O(n+1!*n)==> O(permutation of n+1 size*for each we r checking pattern of size n)
+        
+        bool MatchesPattern(string num,string pattern){
+            for(int i=0;i<pattern.size();i++){
+                if(pattern[i]=='I' && num[i]>num[i+1] || pattern[i]=='D' && num[i]<num[i+1]){
+                    return false;
+                }
+            }
+            return true;
+        }
+        string smallestNumber(string pattern) {
+            int n=pattern.size();
+            string num="";
+    
+            for(int i=1;i<=n+1;i++){
+                num.push_back(i+'0');
+            }
+    
+            while(!MatchesPattern(num,pattern)){
+                next_permutation(num.begin(),num.end());
+            }
+    
+            return num;
+            
+            
+        }
+    };
